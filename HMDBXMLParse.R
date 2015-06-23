3e9bad06e1fce3f1992c7b22d18b86504b8356c8
duxiuxia <xdu4@dhcp00094.nri.unc.edu>
2015-06-23 15:04:41 -0400

    Xiuxia edited

--- HMDBXMLParse.R
+++ HMDBXMLParse.R
@@ -1,10 +1,42 @@
-library(XML)
-setwd("C:/Users/matt/Desktop/HMDBMetabolites")
+# Author: Matthew Deitz
 
-metaboliteList<-data.frame(Accession=character(0),Name=character(0),Chemical_Formula=character(0),Avg_Molecular_Weight=character(0),Monisotopic_weight=character(0))
+# Editor: Xiuxia Du
+
+# Started in June 2015
+
+
+
+
+if (!is.element("XML", installed.packages()[,1])) {
+    install.packages("XML")
+}
+
+
+library(XML)
+
+
+
+metaboliteList<-data.frame(Accession=character(0),
+                           Name=character(0),
+                           Chemical_Formula=character(0),
+                           Avg_Molecular_Weight=character(0),
+                           Monisotopic_weight=character(0))
+
+
+
+
 for (infile in dir(getwd(),pattern="*.xml")){
   data<-xmlParse(infile)
+  
   xml_data<-xmlToList(data)
-  metaboliteList<-rbind(metaboliteList,data.frame(Accession=xml_data[[4]],Name=xml_data[[6]],Chemical_Formula=xml_data[[9]],Avg_Molecular_Weight=xml_data[[10]],Monisotopic_weight=xml_data[[11]]))
+  
+  metaboliteList<-rbind(metaboliteList,
+                        data.frame(Accession=xml_data[["accession"]],
+                                   Name=xml_data[["name"]],
+                                   Chemical_Formula=xml_data[["chemical_formula"]],
+                                   Avg_Molecular_Weight=xml_data[["average_molecular_weight"]],
+                                   Monisotopic_weight=xml_data[["monisotopic_moleculate_weight"]]))
 }
+
+
 write.csv(metaboliteList,"HMDBMetaboliteList.csv")
