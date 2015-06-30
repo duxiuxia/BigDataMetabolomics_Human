919e212c48db9b1e0d0dd0f095cfbbdf50b7dbb8
duxiuxia <dulab.binf@gmail.com>
2015-06-29 23:34:40 -0700

    Xiuxia edit

--- QualityThresholdClustering.R
+++ QualityThresholdClustering.R
@@ -1,7 +1,37 @@
+# Author: Matthew Deitz
+
+# Editor: Xiuxia Du
+
+# Started in June 2015
+
+
+
+
+rm(list=rm())
+
+
 PPMTolerance=10
 
+
+
+if ( !is.element("xlsx", installed.packages()[,1]) ) {
+    install.packages("xlsx")
+}
+
 library(xlsx)
-setwd("C:/Users/matt/Desktop/XCMSResultsNutritionStudy")
+
+
+
+
+# ==============================================================
+# set working directory here
+# ==============================================================
+
+
+
+
+
+
 data1=read.xlsx("UnknownMetabolitesMZValues.xlsx",sheetName="NV1")
 data2=read.xlsx("UnknownMetabolitesMZValues.xlsx",sheetName="NV2")
 data3=read.xlsx("UnknownMetabolitesMZValues.xlsx",sheetName="NV3")
@@ -11,21 +41,29 @@ data6=read.xlsx("UnknownMetabolitesMZValues.xlsx",sheetName="NV6")
 data7=read.xlsx("UnknownMetabolitesMZValues.xlsx",sheetName="NV7")
 #Reads in the data from the worksheet
 
-mzvalues = rbind(data1,data2,data3,data4,data5,data6,data7)
+
+
+
+
+mzvalues <- rbind(data1,data2,data3,data4,data5,data6,data7)
 #combines all the rows into one dataset
 
-mzvalues = mzvalues[ with(mzvalues,order(Query_mz)),]
+
+
+mzvalues <- mzvalues[ with(mzvalues,order(Query_mz)),]
 #Orders the mzvalues by the Query value
 
 
 results<-list()
 endFlag=T
 
-while (endFlag){
-  index=findSmallestDist(mzvalues)
+while (endFlag) {
+  index <- findSmallestDist_2(mzvalues)
+  
   if (((mzvalues[index+1]-mzvalues[index])/mzvalues[index]*1000000)<PPMTolerance){
     
     indexValues<-extendCluster(mzvalues,index,index+1,PPMTolerance)
+    
     smallIndex=indexValues[1]
     largeIndex=indexValues[2]
     cluster = mzvalues[smallIndex:largeIndex]
@@ -43,6 +81,21 @@ while (endFlag){
   }
 }
 
+
+
+
+
+findSmallestDist_2 <- function(x) {
+    x1 <- x[-length(x)]
+    x2 <- x[-1]
+    all_distance <- x2 - x1
+    II <- which(all_distance == min(all_distance))
+    return(II)
+}
+
+
+
+
 findSmallestDist<-function(mzvalues){
   i=2
   smallestDist=mzvalues[2]-mzvalues[1]
@@ -57,12 +110,17 @@ findSmallestDist<-function(mzvalues){
   return(smallestIndex)
 }
 
-extendCluster<-function(mzvalues,smallIndex,largeIndex,PPMTolerance){
-  returnSmallIndex=smallIndex
-  returnLargeIndex=largeIndex
+
+
+
+extendCluster <- function(mzvalues,smallIndex,largeIndex,PPMTolerance){
+  returnSmallIndex <- smallIndex
+  returnLargeIndex <- largeIndex
 
   if (returnSmallIndex-1==0){
-    if (((mzvalues[returnSmallIndex]+mzvalues[returnLargeIndex+1])/2-mzvalues[returnSmallIndex])/mzvalues[returnSmallIndex]*1000000<PPMTolerance){
+    if (((mzvalues[returnSmallIndex]+mzvalues[returnLargeIndex+1])/2
+         -mzvalues[returnSmallIndex])/mzvalues[returnSmallIndex]
+        *1000000<PPMTolerance){
       return(extendCluster(mzvalues,returnSmallIndex,returnLargeIndex+1,PPMTolerance))
     }
     else{
@@ -71,7 +129,9 @@ extendCluster<-function(mzvalues,smallIndex,largeIndex,PPMTolerance){
   }
   
   if (returnLargeIndex+1==length(mzvalues)){
-    if (((mzvalues[returnSmallIndex-1]+mzvalues[returnLargeIndex])/2-mzvalues[returnSmallIndex-1])/mzvalues[returnSmallIndex-1]*1000000<PPMTolerance){
+    if (((mzvalues[returnSmallIndex-1]+mzvalues[returnLargeIndex])/2
+         -mzvalues[returnSmallIndex-1])/mzvalues[returnSmallIndex-1]
+        *1000000<PPMTolerance){
       return(extendCluster(mzvalues,returnSmallIndex-1,returnLargeIndex,PPMTolerance))
     }
     else{
@@ -83,7 +143,9 @@ extendCluster<-function(mzvalues,smallIndex,largeIndex,PPMTolerance){
   upperDist=mzvalues[returnLargeIndex+1]-mzvalues[returnLargeIndex]
   
   if (lowerDist<upperDist){
-    if (((mzvalues[returnSmallIndex-1]+mzvalues[returnLargeIndex])/2-mzvalues[returnSmallIndex-1])/mzvalues[returnSmallIndex-1]*1000000<PPMTolerance){
+    if (((mzvalues[returnSmallIndex-1]+mzvalues[returnLargeIndex])/2
+         -mzvalues[returnSmallIndex-1])/mzvalues[returnSmallIndex-1]
+        *1000000<PPMTolerance){
       return(extendCluster(mzvalues,returnSmallIndex-1,returnLargeIndex,PPMTolerance))
     }
     else{
@@ -91,7 +153,9 @@ extendCluster<-function(mzvalues,smallIndex,largeIndex,PPMTolerance){
     }
   }
   else{
-    if (((mzvalues[returnSmallIndex]+mzvalues[returnLargeIndex+1])/2-mzvalues[returnSmallIndex])/mzvalues[returnSmallIndex]*1000000<PPMTolerance){
+    if (((mzvalues[returnSmallIndex]+mzvalues[returnLargeIndex+1])/2
+         -mzvalues[returnSmallIndex])/mzvalues[returnSmallIndex]
+        *1000000<PPMTolerance){
       return(extendCluster(mzvalues,returnSmallIndex,returnLargeIndex+1,PPMTolerance))
     }
     else{
