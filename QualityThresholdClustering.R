3264e3118b79e22d5bde5b0b433af0eb811e910b
deitzmatt <mdeitz@uncc.edu>
2015-07-23 15:48:02 -0400

    changes

--- QualityThresholdClustering.R
+++ QualityThresholdClustering.R
@@ -45,41 +45,11 @@ data=read.csv("HMDBMetaboliteList.csv")
 
 #Reads in the data from the worksheet
 mzvalues <- sort(data$Monisotopic_weight)
-
-mzvalues <- mzvalues[ with(mzvalues,order(Query_mz)),]
 #Orders the mzvalues by the Query value
 
 
 results<-list()
 endFlag=T
-
-while (endFlag) {
-  index <- findSmallestDist_2(mzvalues)
-  
-  if (((mzvalues[index+1]-mzvalues[index])/mzvalues[index]*1000000)<PPMTolerance){
-    
-    indexValues<-extendCluster(mzvalues,index,index+1,PPMTolerance)
-    
-    smallIndex=indexValues[1]
-    largeIndex=indexValues[2]
-    cluster = mzvalues[smallIndex:largeIndex]
-    
-    results[[length(results)+1]]<-cluster
-    
-    removeValues=c(smallIndex:largeIndex)
-    mzvalues<-mzvalues[-removeValues]
-    
-    
-  }
-  else{
-    endFlag=F
-    results<-c(results,mzvalues)
-  }
-}
-
-
-
-
 
 findSmallestDist_2 <- function(x) {
     x1 <- x[-length(x)]
@@ -141,8 +111,30 @@ extendCluster <- function(mzvalues,smallIndex,largeIndex,PPMTolerance){
   }
 }
 
-xx<-lapply(results,unlist)
-max<-max(sapply(xx,length))
-
-
-write.csv(do.call(rbind,lapply(xx,function(z)c(z,rep(NA,max-length(z))))),"QTClusterResults.csv")
+while (endFlag) {
+    index <- findSmallestDist_2(mzvalues)
+    if(length(index)>1){
+        index <- index[1]
+    }
+    
+    if (((mzvalues[index+1]-mzvalues[index])/mzvalues[index]*1000000)<PPMTolerance){
+        
+        indexValues<-extendCluster(mzvalues,index,index+1,PPMTolerance)
+        
+        smallIndex=indexValues[1]
+        largeIndex=indexValues[2]
+        
+        results[[length(results)+1]]<-(mzvalues[smallIndex]+mzvalues[largeIndex])/2
+        
+        removeValues=c(smallIndex:largeIndex)
+        mzvalues<-mzvalues[-removeValues]
+        
+        
+    }
+    else{
+        endFlag=F
+        results<-c(results,mzvalues)
+    }
+}
+results <- data.frame(MZValues = unlist(results))
+write.csv(results,"HMDBQTClusterResults.csv")
